b776969481bb6094f0f086758b3b95a5daa7250e
Ahmed Abdelkafi <ahmed.abdelkafi@bitgrass.com>
2025-07-07 12:45:21 +0000

    update image removal

--- app/(components)/(contentlayout)/projects/project-list/page.tsx
+++ app/(components)/(contentlayout)/projects/project-list/page.tsx
@@ -11,6 +11,11 @@ const Select = dynamic(() => import("react-select"), { ssr: false });
 const Projectslist = () => {
     const [searchTerm, setSearchTerm] = useState(""); // State for search input
     const [filteredData, setFilteredData] = useState(ProjectListdata); // State for filtered data
+    const svgRemoval = `<svg xmlns="http://www.w3.org/2000/svg" width="18" height="24" style="fill: none; stroke: rgb(var(--primary));" viewBox="0 0 24 24">
+                                                                <path d="M8 17L12 21L16 17" stroke:rgb(var(--primary));" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                                                <path d="M12 12V21" stroke-width="2" stroke:rgb(var(--primary));" stroke-linecap="round" stroke-linejoin="round"/>
+                                                                <path d="M20.8802 18.0899C21.7496 17.4786 22.4015 16.6061 22.7415 15.5991C23.0814 14.5921 23.0916 13.503 22.7706 12.4898C22.4496 11.4766 21.814 10.592 20.9562 9.9645C20.0985 9.33697 19.063 8.9991 18.0002 8.99993H16.7402C16.4394 7.82781 15.8767 6.73918 15.0943 5.81601C14.3119 4.89285 13.3303 4.15919 12.2234 3.67029C11.1164 3.18138 9.91302 2.94996 8.7037 2.99345C7.49439 3.03694 6.31069 3.3542 5.24173 3.92136C4.17277 4.48852 3.2464 5.29078 2.53236 6.26776C1.81833 7.24474 1.33523 8.37098 1.11944 9.56168C0.903647 10.7524 0.960787 11.9765 1.28656 13.142C1.61233 14.3074 2.19824 15.3837 3.00018 16.2899" stroke:rgb(var(--primary));" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                                            </svg>`
 
     // Handle search input changes
     const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -133,13 +138,12 @@ const Projectslist = () => {
                                                     <td>{idx.standard}</td>
                                                     <td>{idx.location}</td>
                                                     <td>
-                                                        <span className={`badge rounded-full gap-2`} style={{ background: "#F3F6F8", color: "#333335" }}>
-
-                                                            <img
-                                                                src="../../../assets/images/svg/removal.svg"
-                                                                alt="status icon"
-                                                                className="w-4 h-4"
-                                                            />Removal</span>
+                                                        <div className={`badge rounded-full gap-2`} style={{ background: "#F3F6F8", color: "#333335" , paddingTop:"0.25rem" , paddingBottom:"0.25rem" }}>
+                                                            <span
+                                                                className="w-4"
+                                                                dangerouslySetInnerHTML={{ __html: svgRemoval }}
+                                                            />
+                                                            Removal</div>
                                                     </td>
 
                                                 </tr>
--- shared/data/apps/jobs/ProjectListdata.tsx
+++ shared/data/apps/jobs/ProjectListdata.tsx
@@ -8,10 +8,10 @@ interface ProjectListData {
     class: string;
     class1: string;
     src: string;
-    area :string;
+    area: string;
     text1: string;
-    statusFontColor:string;
-    statusBgColor:string;
+    statusFontColor: string;
+    statusBgColor: string;
     color1: string;
     class2: string; // This is an SVG path; consider renaming to svgPath
     class3: string;
@@ -53,38 +53,43 @@ const checked = <input className="form-check-input" type="checkbox" id="checkbox
 const notchecked = <input className="form-check-input" type="checkbox" id="checkboxNoLabelproject2" aria-label="..." defaultValue="" />
 export const ProjectListdata: ProjectListData[] = [
     {
-        id: 1, 
-        class: 'Bitgrass Farmland', 
-        class1: '100 Hectares', 
-        src: "../../../assets/images/faces/faviconDark1.png", 
-        text1: 'Puro', 
-        statusFontColor:"#23B7E5",
-        statusBgColor:"#23B7E51A",
+        id: 1,
+        class: 'Bitgrass Farmland',
+        class1: '100 Hectares',
+        src: "../../../assets/images/faces/faviconDark1.png",
+        text1: 'Puro',
+        statusFontColor: "#23B7E5",
+        statusBgColor: "#23B7E51A",
         color1: 'secondary',
-        class2: 'M21.46777,2.3252A1.00007,1.00007,0,0,0,20.73,2H3.27a1.00039,1.00039,0,0,0-.99609,1.08887l1.52,17a.99944.99944,0,0,0,.72851.87451l7.2002,2A.99628.99628,0,0,0,11.99023,23a1.01206,1.01206,0,0,0,.26709-.03613l7.21973-2a1.00055,1.00055,0,0,0,.729-.875l1.52-17A1,1,0,0,0,21.46777,2.3252Zm-3.19238,16.896L11.99072,20.9624,5.72461,19.22168,4.36328,4H19.63672ZM7.81982,13h6.895l-.32714,3.271-2.56788.917L8.65625,16.05811a1.00017,1.00017,0,1,0-.67285,1.88378l3.5,1.25a1.00291,1.00291,0,0,0,.67285,0l3.5-1.25a1.00044,1.00044,0,0,0,.65869-.84228l.5-5A1.00064,1.00064,0,0,0,15.81982,11H8.72461L8.4248,8h7.895a1,1,0,0,0,0-2h-9a1.00064,1.00064,0,0,0-.99511,1.09961l.5,5A1.00012,1.00012,0,0,0,7.81982,13Z', 
-        class3: '', 
-        checked: notchecked, 
-        data: 'Full Time', 
-        data1: 'Oct 12 2022', 
-        text: 'Biomass Removal', 
-        text2: 'Nov 12 2022', 
-        color: 'primary', 
-        standard: '-', 
+        class2: 'M21.46777,2.3252A1.00007,1.00007,0,0,0,20.73,2H3.27a1.00039,1.00039,0,0,0-.99609,1.08887l1.52,17a.99944.99944,0,0,0,.72851.87451l7.2002,2A.99628.99628,0,0,0,11.99023,23a1.01206,1.01206,0,0,0,.26709-.03613l7.21973-2a1.00055,1.00055,0,0,0,.729-.875l1.52-17A1,1,0,0,0,21.46777,2.3252Zm-3.19238,16.896L11.99072,20.9624,5.72461,19.22168,4.36328,4H19.63672ZM7.81982,13h6.895l-.32714,3.271-2.56788.917L8.65625,16.05811a1.00017,1.00017,0,1,0-.67285,1.88378l3.5,1.25a1.00291,1.00291,0,0,0,.67285,0l3.5-1.25a1.00044,1.00044,0,0,0,.65869-.84228l.5-5A1.00064,1.00064,0,0,0,15.81982,11H8.72461L8.4248,8h7.895a1,1,0,0,0,0-2h-9a1.00064,1.00064,0,0,0-.99511,1.09961l.5,5A1.00012,1.00012,0,0,0,7.81982,13Z',
+        class3: '',
+        checked: notchecked,
+        data: 'Full Time',
+        data1: 'Oct 12 2022',
+        text: 'Biomass Removal',
+        text2: 'Nov 12 2022',
+        color: 'primary',
+        standard: '-',
         number: '10', number1: '15',
         name: "Bitgrass Farmland",
         location: "Tunisia",
         status: "Under Developement",
-        area : "100 Hecrares" ,
+        area: "100 Hecrares",
         daysLeftToInvest: 45,
         longitude: 10.7603,
         latitude: 34.7406,
         zoom: 13,
+
+
+
         projectData: [
             {
-                title: "Removal", subtitle: "Credit Type", icon: ` <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                                        style="fill: rgb(var(--primary));" viewBox="0 0 24 24" >
-                                                        <path d="m5,15h5v6c0,.43.28.82.69.95.1.03.21.05.31.05.31,0,.62-.15.81-.41l8-11c.22-.3.25-.71.08-1.04-.17-.34-.52-.55-.89-.55h-5V3c0-.43-.28-.82-.69-.95-.41-.13-.86.01-1.12.36L4.19,13.41c-.22.3-.25.71-.08,1.04.17.34.52.55.89.55Zm7-8.92v3.92c0,.55.45,1,1,1h4.04l-5.04,6.92v-3.92c0-.55-.45-1-1-1h-4.04l5.04-6.92Z"></path>
-                                                    </svg>` },
+                title: "Removal", subtitle: "Credit Type", icon: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" style="fill: none; stroke: rgb(var(--primary));" viewBox="0 0 24 24">
+  <path d="M8 17L12 21L16 17" stroke: rgb(var(--primary));" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M12 12V21" stroke-width="2" stroke: rgb(var(--primary));" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M20.8802 18.0899C21.7496 17.4786 22.4015 16.6061 22.7415 15.5991C23.0814 14.5921 23.0916 13.503 22.7706 12.4898C22.4496 11.4766 21.814 10.592 20.9562 9.9645C20.0985 9.33697 19.063 8.9991 18.0002 8.99993H16.7402C16.4394 7.82781 15.8767 6.73918 15.0943 5.81601C14.3119 4.89285 13.3303 4.15919 12.2234 3.67029C11.1164 3.18138 9.91302 2.94996 8.7037 2.99345C7.49439 3.03694 6.31069 3.3542 5.24173 3.92136C4.17277 4.48852 3.2464 5.29078 2.53236 6.26776C1.81833 7.24474 1.33523 8.37098 1.11944 9.56168C0.903647 10.7524 0.960787 11.9765 1.28656 13.142C1.61233 14.3074 2.19824 15.3837 3.00018 16.2899" stroke: rgb(var(--primary));" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+` },
             {
                 title: "60K TCO2", subtitle: "Carbon Units", icon: ` <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
                                                          style="fill: rgb(var(--primary));" viewBox="0 0 24 24" >
