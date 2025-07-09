771346b9381804fdbfc550458224a6e3b46c635a
Ahmed Abdelkafi <ahmed.abdelkafi@bitgrass.com>
2025-07-09 09:47:04 +0000

    fix farcaster

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -1,44 +1,52 @@
- 
+
 function withValidProperties(
-    properties: Record<string, undefined | string | string[]>,
-  ) {
-    return Object.fromEntries(
-      Object.entries(properties).filter(([key, value]) => {
-        if (Array.isArray(value)) {
-          return value.length > 0;
-        }
-        return !!value;
-      }),
-    );
-  }
-   
-  export async function GET() {
-    const URL = process.env.NEXT_PUBLIC_URL;
-   
-    return Response.json({
-      accountAssociation: {
-        header: process.env.FARCASTER_HEADER,
-        payload: process.env.FARCASTER_PAYLOAD,
-        signature: process.env.FARCASTER_SIGNATURE,
-      },
-      frame: withValidProperties({
-        version: "1",
-        name: process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME,
-        subtitle: process.env.NEXT_PUBLIC_APP_SUBTITLE,
-        description: process.env.NEXT_PUBLIC_APP_DESCRIPTION,
-        screenshotUrls: [],
-        iconUrl: process.env.NEXT_PUBLIC_APP_ICON,
-        splashImageUrl: process.env.NEXT_PUBLIC_APP_SPLASH_IMAGE,
-        splashBackgroundColor: process.env.NEXT_PUBLIC_SPLASH_BACKGROUND_COLOR,
-        homeUrl: URL,
-        webhookUrl: `${URL}/api/webhook`,
-        primaryCategory: process.env.NEXT_PUBLIC_APP_PRIMARY_CATEGORY,
-        tags: [],
-        heroImageUrl: process.env.NEXT_PUBLIC_APP_HERO_IMAGE,
-        tagline: process.env.NEXT_PUBLIC_APP_TAGLINE,
-        ogTitle: process.env.NEXT_PUBLIC_APP_OG_TITLE,
-        ogDescription: process.env.NEXT_PUBLIC_APP_OG_DESCRIPTION,
-        ogImageUrl: process.env.NEXT_PUBLIC_APP_OG_IMAGE,
-      }),
-    });
-  }
+  properties: Record<string, undefined | string | string[]>,
+) {
+  return Object.fromEntries(
+    Object.entries(properties).filter(([key, value]) => {
+      if (Array.isArray(value)) {
+        return value.length > 0;
+      }
+      return !!value;
+    }),
+  );
+}
+
+export async function GET() {
+  const URL = process.env.NEXT_PUBLIC_URL;
+
+  return Response.json({
+    accountAssociation: {
+      header: process.env.FARCASTER_HEADER,
+      payload: process.env.FARCASTER_PAYLOAD,
+      signature: process.env.FARCASTER_SIGNATURE,
+    },
+    frame: withValidProperties({
+      version: "1",
+      name: process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME,
+      subtitle: process.env.NEXT_PUBLIC_APP_SUBTITLE,
+      description: process.env.NEXT_PUBLIC_APP_DESCRIPTION,
+      screenshotUrls: [
+        "https://ik.imagekit.io/cafu/bitgrass/100m2Share.webp?updatedAt=1751037776589&ik-s=a2715b2b9d2a1d53e36830c47212d2fcc58f28bf",
+        "https://ik.imagekit.io/cafu/bitgrass/500m2Share.webp?updatedAt=1751037775707&ik-s=0a4a436924505de65a90c3c7f6075d0e132426da", 
+        "https://ik.imagekit.io/cafu/bitgrass/1000m2Share.webp?updatedAt=1751037779175&ik-s=aaa06f48d035369ec9782a489ea3b2c59fc7753e"],
+      iconUrl: process.env.NEXT_PUBLIC_APP_ICON,
+      splashImageUrl: process.env.NEXT_PUBLIC_APP_SPLASH_IMAGE,
+      splashBackgroundColor: process.env.NEXT_PUBLIC_SPLASH_BACKGROUND_COLOR,
+      homeUrl: URL,
+      webhookUrl: `${URL}/api/webhook`,
+      primaryCategory: process.env.NEXT_PUBLIC_APP_PRIMARY_CATEGORY,
+      tags: [
+        "rewards",
+        "leaderboard",
+        "warpcast",
+        "earn"
+      ],
+      heroImageUrl: process.env.NEXT_PUBLIC_APP_HERO_IMAGE,
+      tagline: process.env.NEXT_PUBLIC_APP_TAGLINE,
+      ogTitle: process.env.NEXT_PUBLIC_APP_OG_TITLE,
+      ogDescription: process.env.NEXT_PUBLIC_APP_OG_DESCRIPTION,
+      ogImageUrl: process.env.NEXT_PUBLIC_APP_OG_IMAGE,
+    }),
+  });
+}
